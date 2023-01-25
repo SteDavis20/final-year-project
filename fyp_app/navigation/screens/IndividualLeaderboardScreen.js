7aa323bc771186a52b3889ac8bae3d398b574463
Stephen Davis <stephendavis2016@gmail.com>
2023-01-25 22:29:03 +0000

    Adding dummy data leaderboards

--- fyp_app/navigation/screens/IndividualLeaderboardScreen.js
+++ fyp_app/navigation/screens/IndividualLeaderboardScreen.js
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import Leaderboard from "react-native-leaderboard";
+
+const data = [
+  { userName: "Sean", highScore: 10 },
+  { userName: "Aaron", highScore: 7 },
+  { userName: "Peadar", highScore: 5 },
+  { userName: "Tim", highScore: 4 },
+  { userName: "Mark", highScore: 2 },
+]; //can also be an object of objects!: data: {a:{}, b:{}}
+
 export default function IndividualLeaderboardScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Individual Leaderboard Screen!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Leaderboard data={data} sortBy="highScore" labelBy="userName" />;
 }
 
 const styles = StyleSheet.create({
--- fyp_app/navigation/screens/TeamLeaderboardScreen.js
+++ fyp_app/navigation/screens/TeamLeaderboardScreen.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import Leaderboard from "react-native-leaderboard";
+
+const data = [
+  { userName: "Team1", highScore: 15 },
+  { userName: "Team2", highScore: 12 },
+  { userName: "Team3", highScore: 8 },
+]; //can also be an object of objects!: data: {a:{}, b:{}};
+
 export default function TeamLeaderboardScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Team Leaderboard Screen!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Leaderboard data={data} sortBy="highScore" labelBy="userName" />;
 }
 
 const styles = StyleSheet.create({
