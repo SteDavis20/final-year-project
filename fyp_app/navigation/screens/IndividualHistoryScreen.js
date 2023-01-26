aac8b304e0028ccdc2c1083a14feee5d275b8dfa
Stephen Davis <stephendavis2016@gmail.com>
2023-01-26 08:42:56 +0000

    Adding line chart and history of user scores

--- fyp_app/navigation/screens/IndividualHistoryScreen.js
+++ fyp_app/navigation/screens/IndividualHistoryScreen.js
@@ -1,11 +1,66 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import Leaderboard from "react-native-leaderboard";
+
+import {
+  LineChart,
+  // BarChart,
+  // PieChart,
+  // ProgressChart,
+  // ContributionGraph,
+  // StackedBarChart,
+} from "react-native-chart-kit";
+
+// To make chart responsive with different screen widths
+import { Dimensions } from "react-native";
+const screenWidth = Dimensions.get("window").width;
+
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false, // optional
+};
+
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      data: [20, 45, 28, 80, 99, 43],
+      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+      strokeWidth: 2, // optional
+    },
+  ],
+  legend: ["Rainy Days"], // optional
+};
+
+const leaderboardData = [
+  { date: "Monday 01/01/23", score: 2 },
+  { date: "Tuesday 02/01/23", score: 7 },
+  { date: "Wednesday 03/01/23", score: 5 },
+  { date: "Thursday 04/01/23", score: 4 },
+  { date: "Friday 05/01/23", score: 6 },
+]; //can also be an object of objects!: data: {a:{}, b:{}}
+
 export default function IndividualHistoryScreen() {
   return (
-    <View style={styles.container}>
+    <View /*style={styles.container}*/>
       <Text>Individual History Screen!</Text>
       <StatusBar style="auto" />
+      <LineChart
+        data={data}
+        width={screenWidth}
+        height={220}
+        chartConfig={chartConfig}
+      />
+      <Text>Need to add spacing here...</Text>
+      {/* sortBy is what is displayed on RHS, need to sort by date, but display score on RHS, not date */}
+      <Leaderboard data={leaderboardData} sortBy="score" labelBy="date" />
     </View>
   );
 }
