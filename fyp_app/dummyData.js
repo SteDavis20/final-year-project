eef7d793ec5eaea54bae65ea105eae078f252031
Stephen Davis <stephendavis2016@gmail.com>
2023-02-09 12:33:37 +0000

    Adding dummydata log entries to homepage

---
+++ fyp_app/dummyData.js
@@ -0,0 +1,29 @@
+const foodDummyData = [
+  {
+    id: "1",
+    name: "Beef",
+    co2e: 12.4,
+  },
+  {
+    id: "2",
+    name: "Chicken",
+    co2e: 3.5,
+  },
+];
+
+const transportDummyData = [
+  {
+    id: "1",
+    name: "Small car",
+    co2e: 4.1,
+  },
+  {
+    id: "2",
+    name: "Bus",
+    co2e: 2.4,
+  },
+];
+
+export { foodDummyData, transportDummyData };
+
+// export default { foodDummyData, transportDummyData };
--- fyp_app/navigation/screens/HomeScreen.js
+++ fyp_app/navigation/screens/HomeScreen.js
@@ -10,6 +10,8 @@ import {
 
 import { PieChart } from "react-native-gifted-charts";
 
+import { foodDummyData, transportDummyData } from "../../dummyData";
+
 export default function HomeScreen() {
   const renderLegend = (text, color) => {
     return (
@@ -27,6 +29,7 @@ export default function HomeScreen() {
       </View>
     );
   };
+
   return (
     <ScrollView>
       <View
@@ -94,14 +97,65 @@ export default function HomeScreen() {
       </View>
       <Text style={styles.setFontSizeOne}>Home Screen!</Text>
       <StatusBar style="auto" />
-      {/* <Button
-        title="Beef"
-        color="#f194ff"
-        onPress={() => Alert.alert("Cannot press this one")}
-      /> */}
+
+      {/****************************************************************************/}
+      {/* Food Log Entires */}
+      {/* Maybe make these buttons at a later stage, to allow user to edit/delete entry */}
+      <View style={styles1.container}>
+        <ScrollView>
+          <Text style={styles.setFontSizeOne}>Food</Text>
+          <View>
+            {foodDummyData.map((food) => {
+              return (
+                <View>
+                  <Text style={styles1.item}>
+                    {food.name}
+                    {/* Need to replace tabs with aligning co2e to the very right of the screen */}
+                    {"\t"}
+                    {"\t"}
+                    {"\t"}
+                    {food.co2e} gCo2e/kg
+                  </Text>
+                </View>
+              );
+            })}
+          </View>
+          {/****************************************************************************/}
+          {/* Transport Log Entires */}
+          <Text style={styles.setFontSizeOne}>Transport</Text>
+          <View>
+            {transportDummyData.map((transport) => {
+              return (
+                <View>
+                  <Text style={styles1.item}>
+                    {transport.name} {"\t"}
+                    {/* Need to replace tabs with aligning co2e to the very right of the screen */}
+                    {"\t"}
+                    {"\t"}
+                    {transport.co2e} gCo2e/km
+                  </Text>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </View>
     </ScrollView>
   );
 }
+
+// styling for log entries on homepage
+const styles1 = StyleSheet.create({
+  container: {
+    padding: 50,
+    flex: 1,
+  },
+  item: {
+    padding: 20,
+    fontSize: 15,
+    marginTop: 5,
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
